a7c5243a44b1b4f8d864510a7da07dedf11322ae
vishnu.benny <vishnu.benny@htallc.com>
2018-03-30 12:52:59 +0530

    Added user detail model class. Added get request for user detail in api interface.

--- app/src/main/java/com/fabsv/believers/believers/data/source/remote/retrofit/ApiInterface.kt
+++ app/src/main/java/com/fabsv/believers/believers/data/source/remote/retrofit/ApiInterface.kt
@@ -1,11 +1,20 @@
 package com.fabsv.believers.believers.data.source.remote.retrofit
 
 import com.fabsv.believers.believers.data.source.remote.model.User
+import com.fabsv.believers.believers.data.source.remote.model.UserDetail
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
-    @GET("test")
-    fun userLogin(@Query("q") phoneNumber: String): Observable<User>
+    @GET("login")
+    fun userLogin(@Query("phone") phoneNumber: String): Observable<User>
+
+    @GET("getdata")
+    fun getUserData(@Query("qrvalue") qrValue: String): Observable<UserDetail>
+
+    @GET("attendance")
+    fun userAttendance(@Query("phone") phoneNumber: String,
+                       @Query("qrvalue") qrValue: String,
+                       @Query("status") updateStatus: String): Observable<User>
 }
