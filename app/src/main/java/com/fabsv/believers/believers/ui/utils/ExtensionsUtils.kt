ba93dfe699d95198be106f6212215567ed7870e0
vishnu.benny <vishnu.benny@htallc.com>
2018-04-23 15:51:03 +0530

    Default paramter property added in inflate method

--- app/src/main/java/com/fabsv/believers/believers/ui/utils/ExtensionsUtils.kt
+++ app/src/main/java/com/fabsv/believers/believers/ui/utils/ExtensionsUtils.kt
@@ -6,6 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-fun ViewGroup.inflate(layoutId: Int): View {
-    return LayoutInflater.from(context).inflate(layoutId, this, false)
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 }
