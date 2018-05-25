d3bb3f81f3cd51ab5abed6a2336a46f943b3c180
vishnu.benny <vishnu.benny@htallc.com>
2018-05-25 12:55:22 +0530

    Mock api call

--- app/src/main/java/com/fabsv/believers/believers/data/source/remote/retrofit/ApiInterface.kt
+++ app/src/main/java/com/fabsv/believers/believers/data/source/remote/retrofit/ApiInterface.kt
@@ -6,13 +6,14 @@ import retrofit2.Response
 import retrofit2.http.*
 
 interface ApiInterface {
-    @GET("ValidateLogin")
+    @GET("5b07b7363200007e006ffff2")
+//    @GET("ValidateLogin")
     fun userLogin(@Query("UserName") username: String,
                   @Query("Password") password: String,
                   @Query("MobileNumber") mobile: String): Observable<Response<LoginResponse>>
 
-    //    @GET("5afc3f1a3100006e007c5d99")
-    @GET("GetByQRCode")
+        @GET("5afc3f1a3100006e007c5d99")
+//    @GET("GetByQRCode")
     fun getUserProfile(@Query("QRCode") qrValue: String,
                        @Query("MandalamId") mandalamId: String?,
                        @Query("MeetingSlno") meetingSlNo: String?): Observable<Response<UserProfileResponse>>
@@ -21,15 +22,15 @@ interface ApiInterface {
     @FormUrlEncoded
     fun makeAttendancePresent(@Body makeAttendancePresentModel: MakeAttendancePresentModel?): Observable<Response<Any>>
 
-//    @GET("5b0540623200008100ebf7b1")
-    @GET("CollectionSummary")
+    @GET("5b0540623200008100ebf7b1")
+//    @GET("CollectionSummary")
     fun getCollectionReport(@Query("MandalamId") mandalamId: String,
                             @Query("MeetingSlno") meetingSlNo: String,
                             @Query("UserId") userId: String,
                             @Query("MobileNo") mobile: String): Observable<Response<CollectionReportResponse>>
 
-//        @GET("5b040f1f2f0000e017e7a869")
-    @GET("Quorum")
+        @GET("5b040f1f2f0000e017e7a869")
+//    @GET("Quorum")
     fun getQuorumReport(@Query("MandalamId") mandalamId: String,
                         @Query("MeetingSlno") meetingSlNo: String,
                         @Query("QuorumTime") dateTimeObject: String): Observable<Response<QuorumReportResponse>>
