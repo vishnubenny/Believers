83e99262007f11222cd2725fac31cc485dcc43dc
vishnu.benny <vishnu.benny@htallc.com>
2018-06-21 13:46:43 +0530

    Updated the saving call for user name in preference

--- app/src/main/java/com/fabsv/believers/believers/data/source/local/prefs/AppPreferencesHelper.kt
+++ app/src/main/java/com/fabsv/believers/believers/data/source/local/prefs/AppPreferencesHelper.kt
@@ -6,13 +6,20 @@ import com.fabsv.believers.believers.util.constants.AppConstants
 import com.fabsv.believers.believers.util.prefs.SharedPreferenceManager
 
 class AppPreferencesHelper(context: Context) {
-    private var sharedPreferencesManager: SharedPreferenceManager? = null
+    private var sharedPreferencesManager: SharedPreferenceManager
+    /*
+    Pref keys listing
+     */
     private var PREF_KEY_IS_LOGGED_IN: String = "PREF_KEY_IS_LOGGED_IN"
     private var PREF_KEY_LOGGED_IN_USER_PHONE_NUMBER: String = "PREF_KEY_LOGGED_IN_USER_PHONE_NUMBER"
     private var PREF_KEY_LOGGED_IN_USER_USERNAME: String = "PREF_KEY_LOGGED_IN_USER_USERNAME"
     private var PREF_KEY_USER_ID: String = "PREF_KEY_USER_ID"
     private var PREF_KEY_MANDALAM_ID: String = "PREF_KEY_MANDALAM_ID"
     private var PREF_KEY_MAEETING_SL_NO: String = "PREF_KEY_MAEETING_SL_NO"
+    /*
+    Default string values
+     */
+    private var DEFAULT_STRING_VALUE: String = ""
 
     init {
         this.sharedPreferencesManager = SharedPreferenceManager(context, AppConstants.Prefs.APP_PREFERENCES)
@@ -30,49 +37,48 @@ class AppPreferencesHelper(context: Context) {
     }
 
     fun isLoggedIn(): Boolean {
-        return sharedPreferencesManager!!.getValue(PREF_KEY_IS_LOGGED_IN, false);
+        return sharedPreferencesManager.getValue(PREF_KEY_IS_LOGGED_IN, false);
     }
 
     fun setLoggedIn(validLogin: Boolean) {
-        sharedPreferencesManager!!.setValue(PREF_KEY_IS_LOGGED_IN, validLogin)
+        sharedPreferencesManager.setValue(PREF_KEY_IS_LOGGED_IN, validLogin)
     }
 
-    fun setLoggedInUserUsername(username: String) {
-        sharedPreferencesManager?.setValue(PREF_KEY_LOGGED_IN_USER_USERNAME, username)
-    }
+    fun setLoggedInUserUsername(username: String) =
+            sharedPreferencesManager.setValue(PREF_KEY_LOGGED_IN_USER_USERNAME, username)
 
-    fun getLoggedInUserUsername() = sharedPreferencesManager?.getValue(PREF_KEY_LOGGED_IN_USER_USERNAME, "")
+    fun getLoggedInUserUsername() =
+            sharedPreferencesManager.getValue(PREF_KEY_LOGGED_IN_USER_USERNAME, defaultValue = DEFAULT_STRING_VALUE)
 
-    fun setLoggedInUserMobile(phoneNumberFieldValue: String) {
-        sharedPreferencesManager!!.setValue(PREF_KEY_LOGGED_IN_USER_PHONE_NUMBER, phoneNumberFieldValue)
-    }
+    fun setLoggedInUserMobile(phoneNumberFieldValue: String) =
+            sharedPreferencesManager.setValue(PREF_KEY_LOGGED_IN_USER_PHONE_NUMBER, phoneNumberFieldValue)
 
-    fun getLoggedInUserPhoneNumber(): String {
-        return sharedPreferencesManager!!.getValue(PREF_KEY_LOGGED_IN_USER_PHONE_NUMBER, "")
-    }
+    fun getLoggedInUserPhoneNumber() =
+            sharedPreferencesManager.getValue(PREF_KEY_LOGGED_IN_USER_PHONE_NUMBER, defaultValue = DEFAULT_STRING_VALUE)
 
     private fun setUserId(userId: Int?) {
-        sharedPreferencesManager?.setValue(PREF_KEY_USER_ID, userId)
+        sharedPreferencesManager.setValue(PREF_KEY_USER_ID, userId)
     }
 
-    private fun getUserId() = sharedPreferencesManager?.getValue(PREF_KEY_USER_ID, 0)
+    private fun getUserId() = sharedPreferencesManager.getValue(PREF_KEY_USER_ID, 0)
 
     private fun setMandalamId(mandalamId: Int?) {
-        sharedPreferencesManager?.setValue(PREF_KEY_MANDALAM_ID, mandalamId)
+        sharedPreferencesManager.setValue(PREF_KEY_MANDALAM_ID, mandalamId)
     }
 
-    private fun getMandalamId() = sharedPreferencesManager?.getValue(PREF_KEY_MANDALAM_ID, 0)
+    private fun getMandalamId() = sharedPreferencesManager.getValue(PREF_KEY_MANDALAM_ID, 0)
 
     private fun setMeetingSlNo(meetingSlNo: Int?) {
-        sharedPreferencesManager?.setValue(PREF_KEY_MAEETING_SL_NO, meetingSlNo)
+        sharedPreferencesManager.setValue(PREF_KEY_MAEETING_SL_NO, meetingSlNo)
     }
 
-    private fun getMeetingSlNo() = sharedPreferencesManager?.getValue(PREF_KEY_MAEETING_SL_NO, 0)
+    private fun getMeetingSlNo() = sharedPreferencesManager.getValue(PREF_KEY_MAEETING_SL_NO, 0)
 
     fun setUserData(loginResponse: LoginResponse) {
         setUserId(loginResponse.userId)
         setMandalamId(loginResponse.mandalamId)
         setMeetingSlNo(loginResponse.meetingSlNo)
+        loginResponse.userName?.let { setLoggedInUserUsername(it) }
     }
 
     fun getUserData(): LoginResponse {
@@ -80,6 +86,7 @@ class AppPreferencesHelper(context: Context) {
         loginResponse.userId = getUserId()
         loginResponse.mandalamId = getMandalamId()
         loginResponse.meetingSlNo = getMeetingSlNo()
+        loginResponse.userName = getLoggedInUserUsername()
         return loginResponse
     }
 }
