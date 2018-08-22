1396bef6d1c88c319579b9f964b2be6e81dd4fe3
vishnu.benny <vishnu.benny@htallc.com>
2018-08-22 13:45:38 +0530

    Comment added

--- app/src/main/java/com/fabsv/believers/believers/util/constants/AppConstants.kt
+++ app/src/main/java/com/fabsv/believers/believers/util/constants/AppConstants.kt
@@ -26,8 +26,8 @@ class AppConstants {
         companion object {
 //            const val BASE_URL = "http://192.168.0.6:8005/api/MandalamMobilApp/"
 //            const val BASE_URL = "http://192.168.0.8:8081/api/MandalamMobilApp/"
-            const val BASE_URL = "http://172.17.17.16:9191/api/MandalamMobilApp/"
-//            const val BASE_URL = "http://172.17.17.14:8071/api/MandalamMobilApp/"
+//            const val BASE_URL = "http://172.17.17.16:9191/api/MandalamMobilApp/"
+            const val BASE_URL = "http://172.17.17.14:8071/api/MandalamMobilApp/" //live
 //            const val BASE_URL = "http://www.mocky.io/v2/"
         }
     }
