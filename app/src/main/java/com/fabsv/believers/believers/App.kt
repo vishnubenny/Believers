96130b5d77a5261091eea54403c417570c92199a
vishnu.benny <vishnu.benny@htallc.com>
2018-08-22 13:45:13 +0530

    Multi-dex enabled

--- app/src/main/java/com/fabsv/believers/believers/App.kt
+++ app/src/main/java/com/fabsv/believers/believers/App.kt
@@ -1,11 +1,18 @@
 package com.fabsv.believers.believers
 
 import android.app.Application
+import android.content.Context
+import android.support.multidex.MultiDex
 import com.fabsv.believers.believers.data.source.local.prefs.AppPreferencesHelper
 
 class App : Application() {
 
     fun getAppPreferencesHelper(): AppPreferencesHelper {
         return AppPreferencesHelper.getInstance(this)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(base)
     }
 }
